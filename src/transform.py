9968b725f8e0b687a223392b0b19d6d51baef143
Ben Heil <ben.jer.heil@gmail.com>
2022-01-03 12:02:42 -0500

    Make transform compatible with tsv formatted plier results

--- src/transform.py
+++ src/transform.py
@@ -6,48 +6,36 @@ expression data into LV space
 import random
 from typing import List
 
-import h5py
 import numpy as np
 import pandas as pd
 
 
 class PlierTransform():
-    def __init__(self, weight_file: str, genes: List[str], debug: bool = False):
+    def __init__(self, weight_file: str, debug: bool = False):
         """
         Load the PLIER weights into a numpy array
 
         Arguments
         ---------
         weight_file: The path to the file output by PLIER storing the weights. By default it will
-                     be called Z.hdf5
-        genes: The genes used PLIER. These genes should be the exact list of genes in the loadings
-               in the same order they are present in the loadings. The easiest way to get these
-               is to parse the file produced by `3_preprocess_expression.py` (which is called
-               no_scrna_tpm.tsv) if you ran the preprocessing scripts via snakemake
+                     be called Z.tsv
         debug: A flag that prints more information about the input data when set to True
         """
-        with h5py.File(weight_file, 'r') as in_file:
-            # TODO change count to counts when moving to recount data
-            if debug:
-                print('Reading loadings...')
-            dataset = in_file.get('count')
+        lv_df = pd.read_csv(weight_file, sep='\t')
+        self.lv_df = lv_df
+        loadings = lv_df.to_numpy()
+        self.loadings = loadings
+        self.file = weight_file
+        self.genes = list(lv_df.index)
 
-            # h5py syntax for 'load the whole array from disk'
-            loadings = dataset[()]
-            # We want the loadings to be genes x LVs but PLIER gives LVs x genes
-            loadings = loadings.T
+        assert len(self.genes) == loadings.shape[0]
 
-            self.file = weight_file
-            self.genes = genes
-
-            assert len(genes) == loadings.shape[0]
-
-            if debug:
-                print('Loading values:')
-                print(loadings)
-                print('Loadings shape: {}'.format(loadings.shape))
-                sparsity = (loadings.size - np.count_nonzero(loadings)) / loadings.size
-                print('Loadings sparsity = {}'.format(sparsity))
+        if debug:
+            print('Loading values:')
+            print(loadings)
+            print('Loadings shape: {}'.format(loadings.shape))
+            sparsity = (loadings.size - np.count_nonzero(loadings)) / loadings.size
+            print('Loadings sparsity = {}'.format(sparsity))
 
             self.loadings = loadings
 
@@ -65,8 +53,7 @@ class PlierTransform():
         plier_expression: A dataframe where the rows are samples and the columns are latent
                          variables
         """
-        reordered_expression = expression[self.genes]
-        print(reordered_expression)
+        reordered_expression = expression[list(self.genes)]
 
         # Ensure there aren't multiple columns with the same gene
         dup_columns = reordered_expression.columns.duplicated()
@@ -88,9 +75,6 @@ class PlierTransform():
 
         return transformed_df
 
-        # Convert genes?
-        # Ensure data is TPM transformed?
-
     def __str__(self):
         """
         Creates a human readable string representation to work with `print`.
@@ -102,16 +86,15 @@ class PlierTransform():
 
 if __name__ == '__main__':
     pathways = pd.read_csv('data/example_pathway_matrix.tsv', sep='\t', index_col=0)
-    genes = (list(pathways.index[:5485]))
 
-    a = PlierTransform('output/Z.hdf5', genes)
+    a = PlierTransform('output/Z.tsv')
     print('Input loadings:')
     print(a)
 
     SAMPLE_COUNT = 1337
 
-    ones = np.ones((SAMPLE_COUNT, 10000))
-    example_genes = random.sample(list(pathways.index[:10000]), k=10000)
+    ones = np.ones((SAMPLE_COUNT, len(pathways.index)))
+    example_genes = pathways.index
     example_samples = ['sample{}'.format(i+1) for i in range(SAMPLE_COUNT)]
     example_expression = pd.DataFrame(ones, columns=example_genes, index=example_samples)
 
