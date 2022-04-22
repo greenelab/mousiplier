ad035395b057e6f13a6f69aa85f0fb60c8daec0b
szhang32 <shuozhang23@gmail.com>
2022-04-22 00:38:59 -0400

    remove commented expression

--- src/11a_reformat_LVs.py
+++ src/11a_reformat_LVs.py
@@ -19,9 +19,7 @@ def main(argv):
         for line in f:
             line = line.strip().split()
             (day, treatment, region, rep) = line[0].split('_')
-            #if treatment == "food":
-            #    continue
-            #print(region)
+
             for i in range(1, LV_count + 1):
                 lv = "LV" + str(i)
                 outfh.write(lv + '\t' + day + '\t' + treatment + '\t' + region + '\t' + str(line[i]) + '\n')
