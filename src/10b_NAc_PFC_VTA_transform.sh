b69be1fc19a9850584f6e48593892873ece93a5a
szhang32 <shuozhang23@gmail.com>
2022-04-22 00:25:26 -0400

    Update and rename NAc_PFC_VTA_transform.sh to 10b_NAc_PFC_VTA_transform.sh

---
+++ src/10b_NAc_PFC_VTA_transform.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+####
+# To transform the gene expression into latent space
+####
+
+
+
+date
+
+
+outfile="../data/NAc_PFC_VTA_Lvs.txt"
+python 10a_NAc_PFC_VTA_transform.py ../output/Z.tsv  ../output/lambda.txt ../data/preprocessed_NAc_PFC_VTA_counts.txt $outfile
+
+date
