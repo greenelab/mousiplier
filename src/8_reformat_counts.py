dc759c57acaedaa0b740ebcf862e3ec339e159a9
szhang32 <shuozhang23@gmail.com>
2022-04-22 00:05:50 -0400

    Update and rename reformat_counts.py to 8_reformat_counts.py

--- src/8_reformat_counts.py
+++ src/8_reformat_counts.py
@@ -3,8 +3,9 @@ This script is used to reformat the count output from featureCount: i.e., combin
 count from different samples
 """
 
-import sys
+
 import pandas as pd
+import argparse
 
 def get_name_map(infile):
     """
@@ -19,20 +20,36 @@ def get_name_map(infile):
             name_map[bamName] = id
     return name_map
 
-def main(argv):
+
+def parse_arguments():
+    """
+    parse the command line argument
+    :return: a parser
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("day1_count", help="the count input for day1 abstinence")
+    parser.add_argument("day28_count", help="the count input for day28 abstinence")
+    parser.add_argument("name_info", help="the map from bam file to sample ID")
+    parser.add_argument("outfile", help="output file")
+
+    return parser
+
+def main():
     """
     :param argv: command line arguments
     the first is day1 count, and the second is day28 count, the third is name_map.txt
     the fourth is output file
     :return:
     """
-    day1_counts = pd.read_csv(argv[0], header = 0, sep='\t', index_col = 0)
-    day2_counts = pd.read_csv(argv[1], header = 0, sep='\t', index_col = 0)
+    parser = parse_arguments()
+    args = parser.parse_args()
+    day1_counts = pd.read_csv(args.day1_count, header = 0, sep='\t', index_col = 0)
+    day2_counts = pd.read_csv(args.day28_count, header = 0, sep='\t', index_col = 0)
     ### transpose so that rows are samples, and columns are genes
     day1_counts_tranposed = day1_counts.T
     day2_counts_tranposed = day2_counts.T
-    name_map = get_name_map(argv[2])
-    outfh = open(argv[3], 'w')
+    name_map = get_name_map(args.name_info)
+    outfh = open(args.outfile, 'w')
 
     if list(day1_counts_tranposed.columns.values) == list(day2_counts_tranposed.columns.values):
         # write header
@@ -65,6 +82,8 @@ def main(argv):
             temp_line = temp_line.strip() + '\n'
             outfh.write(temp_line)
 
+    outfh.close()
+
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
