97c849fe132f212b01ead9e7ff4beb3c68ad38fa
szhang32 <shuozhang23@gmail.com>
2022-04-22 00:13:54 -0400

    change absolute address to relative address

--- src/preprocess_expression.sh
+++ src/preprocess_expression.sh
@@ -7,13 +7,9 @@
 #BSUB -eo preprocess_expression.e
 #BSUB -B
 #BSUB -N
-#BSUB -M 30000
-#BSUB -u Shuo.Zhang@Pennmedicine.upenn.edu
-#BSUB -W 100:00
 
-module add python/3.6.3 
 
-cd /project/eheller_itmat_lab/shuo/Mousiplier/data
-outfile="preprocessed_NAc_PFC_VTA_counts.txt"
 
-python /project/eheller_itmat_lab/shuo/Mousiplier/mousiplier/src/3_preprocess_expression.py NAc_PFC_VTA_counts.txt gene_lengths.tsv plier_pathways.tsv $outfile
+outfile="../data/preprocessed_NAc_PFC_VTA_counts.txt"
+
+python 3_preprocess_expression.py ../data/NAc_PFC_VTA_counts.txt ../data/gene_lengths.tsv ../data/plier_pathways.tsv $outfile
