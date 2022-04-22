86c776e06db7e4461f5273d16a7dbc8a8f077b71
szhang32 <shuozhang23@gmail.com>
2022-04-22 00:36:09 -0400

    Update and rename reformat_LVs.sh to 11b_reformat_LVs.sh

---
+++ src/11b_reformat_LVs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+python 11a_reformat_LVs.py ../data/NAc_PFC_VTA_Lvs.txt ../data/reformated_NAc_PFC_VTA_Lvs.txt
--- src/reformat_LVs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /Users/szhang32/Desktop/Mousiplier/data
-
-python ../scripts/reformat_LVs.py NAc_PFC_VTA_Lvs.txt reformated_NAc_PFC_VTA_Lvs.txt
