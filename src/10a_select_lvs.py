842d59be2d9e604a995ae291df85c780600ae0d9
Ben Heil <ben.jer.heil@gmail.com>
2022-05-17 11:11:56 -0400

    Make select_lvs into a more general script

--- src/10a_select_lvs.py
+++ src/10a_select_lvs.py
@@ -1,53 +1,24 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Select LVs
-# Having 200 LVs is way more manageable than tens of thousands of genes, but that's still a lot of hypotheses to test. Inspired by the discussion section of the MultiPLIER paper, we'll filter out the LVs with low variance in the target dataset under the hypothesis that they are more likely to be reflective of non-brain biology.
-
-# In[1]:
-
+import argparse
 
 import pandas as pd
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('lv_file', help='File containing LVs to be filtered by variance')
+    parser.add_argument('out_file', help='File to store the filtered LVs to ')
+    parser.add_argument('--n_to_keep', help='The number of lvs to keep', default=10)
+    args = parser.parse_args()
 
-# In[2]:
+    lv_df = pd.read_csv(args.lv_file, sep='\t')
+    samples = lv_df['sample']
+    lv_df = lv_df.drop('sample', axis='columns')
+    lv_df = lv_df.sample(frac=1).reset_index(drop=True)
 
+    top_lvs = lv_df.var().nlargest(args.n_to_keep).index
 
-lv_df = pd.read_csv('../output/NAc_PFC_VTA_LVs.txt', sep='\t')
-# Drop information about the individual samples and shuffle to avoid subconcious bias
-lv_df = lv_df.drop('sample', axis='columns')
-lv_df = lv_df.sample(frac=1).reset_index(drop=True)
+    filtered_lvs = lv_df.loc[:,top_lvs]
 
+    filtered_lvs['sample'] = samples
+    filtered_lvs = filtered_lvs.set_index('sample')
 
-# In[3]:
-
-
-lv_df
-
-
-# ### Distribution of LV variances
-
-# In[4]:
-
-
-lv_df.var().hist(bins=50)
-
-
-# There seems to be a cut point between the body of the distribution and its tail at a variance of .01. We'll keep the most variable LVs (those above .01) for the analysis.
-
-# In[5]:
-
-
-lvs_to_keep = lv_df.loc[:,lv_df.var() > .01].columns
-
-
-# ## Save results
-
-# In[6]:
-
-
-lv_df = pd.read_csv('../output/NAc_PFC_VTA_LVs.txt', sep='\t')
-lv_df = lv_df.set_index('sample')
-lv_df = lv_df.loc[:,lvs_to_keep]
-lv_df.to_csv('../output/high_variance_lvs.tsv', sep='\t')
-
+    filtered_lvs.to_csv(args.out_file, sep='\t')
