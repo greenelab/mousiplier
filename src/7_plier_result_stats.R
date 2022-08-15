97fdb24837efb474e5a3172ab2a2a4f6e8fa4a84
Ben Heil <ben.jer.heil@gmail.com>
2022-08-15 14:10:20 -0400

    Update plier result parsing script

--- src/7_plier_result_stats.R
+++ src/7_plier_result_stats.R
@@ -16,7 +16,7 @@ if (rstudioapi::isAvailable()) {
   setwd(dirname(this_file))
 }
 
-plier_results <- readRDS('../output/plier.rds')
+plier_results <- readRDS('../output/filtered_plier.rds')
 
 Z <- plier_results['Z'][[1]]
 U <- plier_results['U'][[1]]
@@ -24,25 +24,25 @@ C <- plier_results['C'][[1]]
 lambda <- plier_results['L2'][[1]]
 
 # Write pathway histogram to file
-png('../output/lv_per_pathway_hist.png', width=720, height=720)
+png('../output/filtered_lv_per_pathway_hist.png', width=720, height=720)
 # Using U != 0 because U isn't a binary matrix, it's just sparse due to L1 norm
 hist(colSums(U != 0), main='Distribution of pathways per LV ', xlab='Pathways per latent variable')
 dev.off()
 
 # Calculate genes per pathway (C)
-png('../output/gene_per_pathway_hist.png', width=720, height=720)
+png('../output/filtered_gene_per_pathway_hist.png', width=720, height=720)
 hist(colSums(C)[colSums(C) < 200], main='Distribution of genes per pathway (truncated at 200)',
      xlab='Genes per pathway', breaks=50)
 dev.off()
 
-png('../output/percent_genes_used_hist.png', width=720, height=720)
+png('../output/filtered_percent_genes_used_hist.png', width=720, height=720)
 hist((colSums(Z != 0) / length(Z[,1])) * 100, breaks=20, main='Percent of all genes used per LV',
      xlab='Percent of genes used')
 dev.off()
 
 # Write results to tsvs
-write.table(Z, file='../output/Z.tsv', sep='\t')
-write.table(U, file='../output/U.tsv', sep='\t')
-fileConn<-file('../output/lambda.txt')
+write.table(Z, file='../output/filtered_Z.tsv', sep='\t')
+write.table(U, file='../output/filtered_U.tsv', sep='\t')
+fileConn<-file('../output/filtered_lambda.txt')
 writeLines(toString(lambda), fileConn)
 close(fileConn)
